fbf24550520884ab85739d8c8a0265cfaf51b1bf
Gustavo Guevara <gustavo.andres.guevara@gmail.com>
2019-03-29 22:05:16 -0500

    add authentication decorators

--- points/views.py
+++ points/views.py
@@ -1,14 +1,16 @@
 # Create your views here.
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.decorators import api_view, authentication_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from points.influx_settings import influx_client
 
 
-@authentication_classes((JWTAuthentication, BasicAuthentication))
 @api_view(['POST'])
+@authentication_classes((JWTAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def save_points(request):
     point_data = request.data
     write = influx_client.write_points(point_data)
